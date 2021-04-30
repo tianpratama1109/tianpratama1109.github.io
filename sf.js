15cb8229bc8ef2ed74a78ecc54fd7b05b58502ae
tianpratama1109 <55422336+tianpratama1109@users.noreply.github.com>
2021-05-01 04:40:56 +0700

    Update sf.js

--- sf.js
+++ sf.js
@@ -1,6 +1,6 @@
 //<![CDATA[
 var setting = {
- exceptionurl : "google.com,bp.blogspot.com,facebook.com,trav4life.site,twitter.com",
+ exceptionurl : "bingobiru.com.google.com,bp.blogspot.com,facebook.com,trav4life.site,twitter.com",
  path : "#?o="
 };
 //]]>
